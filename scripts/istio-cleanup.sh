ba0d782e6d143fd2566d14a99d857d2323dd688e
stefanprodan <stefan.prodan@gmail.com>
2019-04-30 12:34:46 +0300

    Add start, stop and cleanup scripts

---
+++ scripts/istio-cleanup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! -x "$(command -v kubectl)" ]]; then
+    echo "kubectl not found"
+    exit 1
+fi
+
+if [[ ! -x "$(command -v helm)" ]]; then
+    echo "helm not found"
+    exit 1
+fi
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+helm delete --purge istio
+
+kubectl delete -f ${REPO_ROOT}/crd-10.yaml
+kubectl delete -f ${REPO_ROOT}/crd-11.yaml
+
+kubectl delete ns istio-system
+
+
