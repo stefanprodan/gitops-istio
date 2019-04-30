0067f9d2590b1d3a84c6991de5a2890376d61f15
stefanprodan <stefan.prodan@gmail.com>
2019-04-30 14:25:52 +0300

    Add frontend and backend workloads

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,10 +3,13 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 helm delete --purge istio
+helm delete --purge flagger
+helm delete --purge flagger-grafana
 
 kubectl delete -f ${REPO_ROOT}/istio-system/
 
 kubectl delete ns istio-system
 kubectl delete ns prod
 
+kubectl delete crd canaries.flagger.app
 
