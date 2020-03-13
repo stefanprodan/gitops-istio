2beee9ff9fd110b07f837c11396c70e8558d3ba9
stefanprodan <stefan.prodan@gmail.com>
2020-03-13 10:44:06 +0200

    Istio 1.5 and Flagger 1.0 - add Istio operator deployment - install Istio control plane with IstioOperator CR - add Flagger metric templates for Istio telemetry v2 - update canaries to v1beta1

--- scripts/flux-init.sh
+++ scripts/flux-init.sh
@@ -12,11 +12,10 @@ if [[ ! -x "$(command -v helm)" ]]; then
     exit 1
 fi
 
-REPO_GIT_INIT_PATHS="namespaces\,istio-system"
-REPO_GIT_ALL_PATHS="namespaces\,istio-system\,prod"
+REPO_GIT_INIT_PATHS="istio"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 REPO_URL=${1:-git@github.com:stefanprodan/gitops-istio}
-REPO_BRANCH=master
+REPO_BRANCH=istio-1.5
 TEMP=${REPO_ROOT}/temp
 
 rm -rf ${TEMP} && mkdir ${TEMP}
@@ -31,73 +30,45 @@ helm upgrade -i flux fluxcd/flux --wait \
 --set git.path=${REPO_GIT_INIT_PATHS} \
 --set git.pollInterval=1m \
 --set registry.pollInterval=1m \
+--set sync.state=secret \
+--set syncGarbageCollection.enabled=true \
 --namespace flux
-
-cat <<EOF >> ${TEMP}/repositories.yaml
-configureRepositories:
-  enable: true
-  volumeName: repositories-yaml
-  secretName: flux-helm-repositories
-  cacheVolumeName: repositories-cache
-  repositories:
-    - caFile: ""
-      cache: stable-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: stable
-      password: ""
-      url: https://kubernetes-charts.storage.googleapis.com
-      username: ""
-    - caFile: ""
-      cache: istio.io-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: istio.io
-      password: ""
-      url: https://storage.googleapis.com/istio-release/releases/1.4.3/charts
-      username: ""
-    - caFile: ""
-      cache: flagger-index.yaml
-      certFile: ""
-      keyFile: ""
-      name: flagger
-      password: ""
-      url: https://flagger.app
-      username: ""
-EOF
 
 echo ">>> Installing Helm Operator"
 kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
 helm upgrade -i helm-operator fluxcd/helm-operator --wait \
 --set git.ssh.secretName=flux-git-deploy \
 --set helm.versions=v3 \
--f ${TEMP}/repositories.yaml \
 --namespace flux
 
-echo '>>> GitHub deploy key'
+echo ">>> GitHub deploy key"
 kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
 
 # wait until flux is able to sync with repo
-echo ">>> Waiting on user to add above deploy key to Github repo"
+echo ">>> Waiting on user to add above deploy key to Github with write access"
 until kubectl logs -n flux deployment/flux | grep event=refreshed
 do
   sleep 5
 done
-echo "Github deploy key ready"
+echo ">>> Github deploy key is ready"
 
 # wait until sidecar injector webhook is ready before enabled prod namespace on flux
-echo ">>> Waiting for istio sidecar injector to start"
-until kubectl get deploy -n istio-system -l app=sidecarInjectorWebhook | grep "1/1"
+echo ">>> Waiting for istiod to start"
+until kubectl -n istio-system get deploy istiod | grep "1/1"
 do
   sleep 5
 done
-echo "istio sidecar injector ready"
+echo ">>> Istio control plane is ready"
 
-echo ">>> Installing Flux for ${REPO_URL} watching all paths"
+echo ">>> Configuring Flux for ${REPO_URL}"
 helm upgrade -i flux fluxcd/flux --wait \
 --set git.url=${REPO_URL} \
 --set git.branch=${REPO_BRANCH} \
---set git.path=${REPO_GIT_ALL_PATHS} \
+--set git.path="" \
 --set git.pollInterval=1m \
 --set registry.pollInterval=1m \
+--set sync.state=secret \
+--set syncGarbageCollection.enabled=true \
 --namespace flux
+
+echo ">>> Cluster bootstrap done!"
