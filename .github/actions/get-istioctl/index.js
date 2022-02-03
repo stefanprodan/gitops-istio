46c7a9b32c758e09dc8a37d0771806e218aa623b
therealmitchconnors <mitchconnors@gmail.com>
2022-02-03 09:14:04 -0800

    recover sanity

--- .github/actions/get-istioctl/index.js
+++ .github/actions/get-istioctl/index.js
@@ -5,18 +5,18 @@ const semver = require('semver')
 try {
   const myToken = core.getInput('myToken');
   const octokit = github.getOctokit(myToken);
-  const releases = octokit.rest.repos.listReleases({
+  const { data: releases } = octokit.rest.repos.listReleases({
     owner: 'istio',
     repo: 'istio',
   });
   const relmap = new Map();
-  core.setOutput("foo", relmap)
-  // releases.forEach( rel => relmap.set(rel.tag_name, rel));
-  // max = semver.maxSatisfying(Array.from(relmap.keys()), "v1")
-  // core.setOutput("version", max.version);
-  // core.setOutput("major", max.major)
-  // core.setOutput("minor", max.minor)
-  // core.setOutput("patch", max.patch)
+  core.setOutput("foo", releases)
+  releases.forEach( rel => relmap.set(rel.tag_name, rel));
+  max = semver.maxSatisfying(Array.from(relmap.keys()), "v1")
+  core.setOutput("version", max.version);
+  core.setOutput("major", max.major)
+  core.setOutput("minor", max.minor)
+  core.setOutput("patch", max.patch)
 } catch (error) {
   core.setFailed(error.message)
 }
